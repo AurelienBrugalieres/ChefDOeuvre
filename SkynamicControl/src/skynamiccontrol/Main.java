5e9dd2205059f146385ef2e14e2b65bba2273b4d
Fabien Bonneval <fabien.bonneval@gmail.com>
2017-02-13 22:57:09 +0100

    [Main] Add Main class.

---
+++ SkynamicControl/src/skynamiccontrol/Main.java
@@ -0,0 +1,10 @@
+package skynamiccontrol;
+
+/**
+ * Created by fabien on 13/02/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
