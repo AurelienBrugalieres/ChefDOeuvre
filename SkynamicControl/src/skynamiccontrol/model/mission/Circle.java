2484c29fd50b1576939672cad7119e2152f4a869
Fabien Bonneval <fabien.bonneval@gmail.com>
2017-02-14 11:11:03 +0100

    [Insructions] Adds state, and getters ans setters.

--- SkynamicControl/src/skynamiccontrol/model/mission/Circle.java
+++ SkynamicControl/src/skynamiccontrol/model/mission/Circle.java
@@ -6,7 +6,23 @@ import skynamiccontrol.model.Waypoint;
  * Created by fabien on 13/02/17.
  */
 public class Circle extends Instruction {
-    private Waypoint waypoint;
+    private Waypoint center;
     private int radius;
     private Double altitude = null; //if default value overrided. (default value = waypoint1.altitude)
+
+    public Waypoint getCenter() {
+        return center;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public Double getAltitude() {
+        if(altitude == null) {
+            return center.getAltitude();
+        } else {
+            return altitude;
+        }
+    }
 }
--- SkynamicControl/src/skynamiccontrol/model/mission/Instruction.java
+++ SkynamicControl/src/skynamiccontrol/model/mission/Instruction.java
@@ -5,13 +5,44 @@ package skynamiccontrol.model.mission;
  */
 public class Instruction {
     private String name;
-    private String id;
+    private int index;          //index as in the aircraft
     private float duration;
-    private Status status = Status.NOT_SENT;
+    private CommunicationState communicationState = CommunicationState.NOT_SENT;
+    private Status status = Status.PENDING;
 
-    public enum Status {
+    public enum CommunicationState {
         NOT_SENT,
         SENT,
         ACKNOWLEDGED
+    }
+
+    public enum Status {
+        PENDING,
+        RUNNING,
+        DONE
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public float getDuration() {
+        return duration;
+    }
+
+    public CommunicationState getCommunicationState() {
+        return communicationState;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
     }
 }
