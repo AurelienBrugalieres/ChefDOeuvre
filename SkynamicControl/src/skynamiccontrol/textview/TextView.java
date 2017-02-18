bcee8eca606be965c0223209508e7af361330f7b
Fabien-B <fabien.bonneval@gmail.com>
2017-02-18 16:14:26 +0000

    [Test] Adapt test to new environement.

--- SkynamicControl/src/skynamiccontrol/textview/TextView.java
+++ SkynamicControl/src/skynamiccontrol/textview/TextView.java
@@ -54,44 +54,74 @@ public class TextView implements Observer{
 
                 circle = new Circle(new Waypoint(lat, lon, alt, coordinateSystem), radius);
                 circle.setDuration(duration);
-                missionManager.insertInstruction(circle, MissionManager.InsertMode.APPEND);
+                circle.setInsertMode(Instruction.InsertMode.APPEND);
+                missionManager.addInstruction(circle);
                 break;
             case "c1":
                 circle = new Circle(new Waypoint(43.4637222, 1.2751827, 300, Waypoint.CoordinateSystem.LLA), 100);
                 circle.setDuration(20);
-                missionManager.insertInstruction(circle, MissionManager.InsertMode.APPEND);
+                circle.setInsertMode(Instruction.InsertMode.APPEND);
+                missionManager.addInstruction(circle);
                 break;
             case "c2":
                 circle = new Circle(new Waypoint(200,600, 350, Waypoint.CoordinateSystem.LOCAL), 200);
                 circle.setDuration(30);
-                missionManager.insertInstruction(circle, MissionManager.InsertMode.APPEND);
+                circle.setInsertMode(Instruction.InsertMode.APPEND);
+                missionManager.addInstruction(circle);
                 break;
             case "gt1":
                 goToWP = new GoToWP(new Waypoint(43.4637222, 1.2751827, 323, Waypoint.CoordinateSystem.LLA));
                 goToWP.setDuration(50);
-                missionManager.insertInstruction(goToWP, MissionManager.InsertMode.APPEND);
+                goToWP.setInsertMode(Instruction.InsertMode.APPEND);
+                missionManager.addInstruction(goToWP);
                 break;
             case "gt2":
                 goToWP = new GoToWP(new Waypoint(100, 200, 321, Waypoint.CoordinateSystem.LOCAL));
                 goToWP.setDuration(55);
-                missionManager.insertInstruction(goToWP, MissionManager.InsertMode.APPEND);
+                goToWP.setInsertMode(Instruction.InsertMode.APPEND);
+                missionManager.addInstruction(goToWP);
                 break;
-            case "s1":
-                survey = new Survey(43.46, 1.27, 43.4, 1.3, 330, Waypoint.CoordinateSystem.LLA);
-                survey.setDuration(50);
-                missionManager.insertInstruction(survey, MissionManager.InsertMode.APPEND);
-                break;
-            case "s2":
-                survey = new Survey(0, 400, 600, 0, 360, Waypoint.CoordinateSystem.LOCAL);
-                survey.setDuration(50);
-                missionManager.insertInstruction(survey, MissionManager.InsertMode.APPEND);
             case "p1":
                 path = new Path();
                 path.addWaypoint(new Waypoint(43.4637117,1.2756799, 320, Waypoint.CoordinateSystem.LLA));
                 path.addWaypoint(new Waypoint(43.4661319, 1.2768373, 340, Waypoint.CoordinateSystem.LLA));
                 path.addWaypoint(new Waypoint(43.4665644, 1.2726432, 340, Waypoint.CoordinateSystem.LLA));
                 path.setDuration(700);
-                missionManager.insertInstruction(path, MissionManager.InsertMode.APPEND);
+                path.setInsertMode(Instruction.InsertMode.APPEND);
+                missionManager.addInstruction(path);
+                break;
+            case "c1r":
+                circle = new Circle(new Waypoint(43.4637222, 1.2751827, 300, Waypoint.CoordinateSystem.LLA), 100);
+                circle.setDuration(20);
+                circle.setInsertMode(Instruction.InsertMode.REPLACE_NEXTS);
+                missionManager.addInstruction(circle);
+                break;
+            case "c2r":
+                circle = new Circle(new Waypoint(200,600, 350, Waypoint.CoordinateSystem.LOCAL), 200);
+                circle.setDuration(30);
+                circle.setInsertMode(Instruction.InsertMode.REPLACE_NEXTS);
+                missionManager.addInstruction(circle);
+                break;
+            case "gt1r":
+                goToWP = new GoToWP(new Waypoint(43.4637222, 1.2751827, 323, Waypoint.CoordinateSystem.LLA));
+                goToWP.setDuration(50);
+                goToWP.setInsertMode(Instruction.InsertMode.REPLACE_NEXTS);
+                missionManager.addInstruction(goToWP);
+                break;
+            case "gt2r":
+                goToWP = new GoToWP(new Waypoint(100, 200, 321, Waypoint.CoordinateSystem.LOCAL));
+                goToWP.setDuration(55);
+                goToWP.setInsertMode(Instruction.InsertMode.REPLACE_NEXTS);
+                missionManager.addInstruction(goToWP);
+                break;
+            case "p1r":
+                path = new Path();
+                path.addWaypoint(new Waypoint(43.4637117,1.2756799, 320, Waypoint.CoordinateSystem.LLA));
+                path.addWaypoint(new Waypoint(43.4661319, 1.2768373, 340, Waypoint.CoordinateSystem.LLA));
+                path.addWaypoint(new Waypoint(43.4665644, 1.2726432, 340, Waypoint.CoordinateSystem.LLA));
+                path.setDuration(700);
+                path.setInsertMode(Instruction.InsertMode.REPLACE_NEXTS);
+                missionManager.addInstruction(path);
                 break;
             case "n":
                 missionManager.goToNextInstruction();
