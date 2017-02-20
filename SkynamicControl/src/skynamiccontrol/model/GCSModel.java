b663d5ae6f655b7a436603e70df3048a4050841d
Fabien-B <fabien.bonneval@gmail.com>
2017-02-20 15:16:47 +0000

    [Model] Adapt GCSModel to new environnement.

--- SkynamicControl/src/skynamiccontrol/model/GCSModel.java
+++ SkynamicControl/src/skynamiccontrol/model/GCSModel.java
@@ -1,5 +1,6 @@
 package skynamiccontrol.model;
 
+import skynamiccontrol.Timeline.Timeline;
 import skynamiccontrol.view.status.StatusListContainer;
 
 import java.beans.PropertyChangeSupport;
@@ -8,17 +9,19 @@ import java.util.*;
 /**
  * Created by Elodie on 14/02/2017.
  */
-public class GCSModel {
+public class GCSModel extends Observable{
     private final int nb_aircraft;
     private List<Aircraft> aircrafts;
     private StatusManager statusManager;
+    private TimelineManager timelineManager;
   /*  private final Map<EventType, Boolean> eventAvailability;
     private final PropertyChangeSupport support;*/
 
-    public GCSModel(int nb_aircraft, StatusListContainer container) {
-        this.nb_aircraft = nb_aircraft;
+    public GCSModel() {
+        this.nb_aircraft = 0;
         this.aircrafts = new ArrayList<>();
-        statusManager = new StatusManager(this, container);
+        statusManager = new StatusManager(this);
+        timelineManager = new TimelineManager(this);
     }
 
     public List<Aircraft> getAircrafts() {
@@ -29,8 +32,20 @@ public class GCSModel {
         this.aircrafts.add(aircraft);
         //aircraft.addPrivateObserver(this);
         statusManager.createView(aircraft);
+        timelineManager.addAircraft(aircraft);
+        this.setChanged();
+        notifyObservers();
     }
-/*
+
+    public void setStatusListContainer(StatusListContainer statusListContainer) {
+        this.statusManager.setStatusListContainer(statusListContainer);
+    }
+
+    public void setTimeline(Timeline timeline) {
+        this.timelineManager.setTimeline(timeline);
+    }
+
+    /*
     @Override
     public void update(Observable o, Object arg) {
         statusManager.updateView((Aircraft)o);
